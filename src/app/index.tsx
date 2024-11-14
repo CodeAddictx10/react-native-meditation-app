6bd7be312b4ce1511358443ea691df464893524a
CodeAddictx10 <onadeji.samuel@yahoo.com>
2024-11-14 07:56:00 +0100

    ui: home screen meditation list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,14 @@
-import { Text } from "react-native";
+import MeditationListItem from "@/components/MeditationListItem";
+import { meditations } from "@/data";
+import { FlatList } from "react-native";
 
 export default function HomeScreen() {
-  return (
-    <Text className="font-bold text-4xl p-10">HomeScreen</Text>
-  )
+    return (
+        <FlatList
+            data={meditations}
+            contentContainerClassName="gap-5 p-3"
+            className="bg-white"
+            renderItem={({ item }) => <MeditationListItem meditation={item} />}
+        />
+    );
 }
