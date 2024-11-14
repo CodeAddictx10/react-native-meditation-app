c9644822834a4c4d13c12b476dc10fb22bf09443
CodeAddictx10 <onadeji.samuel@yahoo.com>
2024-11-14 08:12:39 +0100

    ui: style meditation list item

--- src/components/MeditationListItem.tsx
+++ src/components/MeditationListItem.tsx
@@ -1,5 +1,7 @@
 import { Text, View } from "react-native";
 import { Meditation } from "@/types";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function MeditationListItem({
     meditation,
@@ -7,8 +9,19 @@ export default function MeditationListItem({
     meditation: Meditation;
 }) {
     return (
-        <View className="p-5 border-2 border-gray-300 rounded-2xl">
-            <Text className="font-semibold text-2xl">{meditation.title}</Text>
+        <View className="flex-row items-center gap-5">
+            <View className="bg-green-700 p-2 rounded-full">
+                <FontAwesome name="check" size={16} color="white" />
+            </View>
+            <View className="flex-1 px-5 py-8 border-2 border-gray-300 rounded-2xl gap-y-4">
+                <Text className="font-semibold text-2xl">
+                    {meditation.title}
+                </Text>
+                <View className="flex-row items-center gap-1">
+                    <FontAwesome6 name="clock" size={16} color="#6B7280" />
+                    <Text className="text-grey-500">{meditation.duration}</Text>
+                </View>
+            </View>
         </View>
     );
 }
