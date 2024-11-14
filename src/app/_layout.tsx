a9922c19f34692aa8405b241ebd377dc2ee9439b
CodeAddictx10 <onadeji.samuel@yahoo.com>
2024-11-14 07:33:34 +0100

    setup expo-router and nativewind

---
+++ src/app/_layout.tsx
@@ -0,0 +1,7 @@
+import { Slot, Stack } from "expo-router";
+import "../../global.css";
+export default function RootLayout() {
+  return <Stack>
+    <Stack.Screen name="index" options={{title: "Meditations"}}/>
+  </Stack>
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <Text className="font-bold text-4xl p-10">HomeScreen</Text>
+  )
+}
